58dba3ff1acfad84be4f3af90dc16d25c93701d1
Chaohui Wang <chaohuiw@google.com>
2023-10-24 23:47:35 +0800

    XiaomiParts: Migrate to CompoundButton.OnCheckedChangeListener
    Switch and SwitchCompat are both CompoundButton.
    
    Using CompoundButton in Java will helps migration in the future.
    
    Bug: 306658427
    Test: manual - check Settings pages
    Test: m RunSettingsLibRoboTests
    Change-Id: I85a70d4c504d8584030ea4a058f30d74206ab835
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- src/co/aospa/parts/display/KcalSettingsFragment.java
+++ src/co/aospa/parts/display/KcalSettingsFragment.java
@@ -7,7 +7,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -16,13 +17,12 @@ import androidx.preference.PreferenceManager;
 import androidx.preference.SeekBarPreference;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import co.aospa.parts.R;
 import co.aospa.parts.display.KcalUtils;
 
 public class KcalSettingsFragment extends PreferenceFragment implements
-        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+        OnPreferenceChangeListener, OnCheckedChangeListener {
 
     private static final String TAG = "KcalSettings";
 
@@ -94,7 +94,7 @@ public class KcalSettingsFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         mKcalSwitchPreference.setChecked(isChecked);
         KcalUtils.writeConfigToNode(KcalUtils.KCAL_ENABLE_NODE, 0, isChecked ? 1 : 0);
     }
--- src/co/aospa/parts/speaker/ClearSpeakerFragment.java
+++ src/co/aospa/parts/speaker/ClearSpeakerFragment.java
@@ -13,16 +13,16 @@ import android.os.Looper;
 import android.util.Log;
 
 import androidx.preference.PreferenceFragment;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import co.aospa.parts.R;
 
 import java.io.IOException;
 
-public class ClearSpeakerFragment extends PreferenceFragment implements OnMainSwitchChangeListener {
+public class ClearSpeakerFragment extends PreferenceFragment implements OnCheckedChangeListener {
 
     private static final String TAG = "ClearSpeakerFragment";
     private static final String PREF_CLEAR_SPEAKER = "clear_speaker_pref";
@@ -41,7 +41,7 @@ public class ClearSpeakerFragment extends PreferenceFragment implements OnMainSw
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         mClearSpeakerPref.setChecked(isChecked);
 
         if (isChecked && startPlaying()) {
